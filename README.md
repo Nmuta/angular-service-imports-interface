ba261426e8b866606abe86443b56ef38f6d352ee
Nmuta <nmuta1@gmail.com>
2019-12-15 10:01:52 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-service-imports-interface
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-service-imports-interface)
---
+++ src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+// here is where we import the car interface
+import { Car } from './car';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: [ './app.component.css' ]
+})
+
+export class AppComponent  {
+
+  // this is where we will rebuild our array of Car types ... coming soon !
+
+}
